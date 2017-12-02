b86794ceee95f1dcd7c805335c516000af993ee8
serv-inc <serv-inc@users.noreply.github.com>
2017-12-02 13:03:20 +0100

    had js logic

--- addon/blockpage.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/* jshint esversion: 6, strict: global */
-/* globals document */
-// licensed under the MPL 2.0 by (github.com/serv-inc)
-
-document.querySelector("#page").innerText = decodeURIComponent(document.location.search.slice(1));
-
