45ff926e27a00ea5bb793af0e74b78a2a0700bb5
serv-inc <serv-inc@users.noreply.github.com>
2019-05-13 12:05:44 +0200

    feat: had older site list
    - did not (semi) auto-update
    - had fixed version of md5, lacked npm package.json

--- Makefile
+++ Makefile
@@ -2,6 +2,15 @@ zip: lint
 	cd addon; zip ../tcf.zip *
 
 lint:
+	cp -u node_modules/blueimp-md5/js/md5.min.js addon/
 	ls addon/*.js | grep -v md5.min.js | xargs jshint
 	python -m json.tool addon/manifest.json > /dev/null
 	tidy -eq addon/blockpage.html
+
+bannedupdate:
+	wget -N https://ahmia.fi/banned
+	sed -i '/^[[:space:]]*$$/d' banned  # double $$ for make
+	sed -i 's/[[:space:]]*//g' banned
+	sed -i 's/^/"/g' banned
+	sed -i 's/$$/",/g' banned
+	mv banned meta/
--- addon/background.js
+++ addon/background.js
@@ -1,9 +1,6 @@
 "use strict";
-/* jshint esversion: 6, strict: global */
-/* jshint laxbreak: true */
-/* globals chrome */
-/* globals document */
-/* globals md5 */
+/* jshint esversion: 6, strict: global, laxbreak: true */
+/* globals chrome, document, md5 */
 // licensed under the MPL 2.0 by (github.com/serv-inc)
 
 const ALLOW = RegExp('(moz|chrome)-extension://');
@@ -31,26 +28,13 @@ function isBanned(host) {
   return BANNED.indexOf(hashval) !== -1;
 }
 
-const BANNED=["02d9e81803b1f68819b7748968bbec3b",
-// "d6f16a08c9098fbfc258f50bae1dfe09", // hash of "p.p" for testing
-"06b8477effc8005b84c1a24c4e2d2793",
+const BANNED=[//"d6f16a08c9098fbfc258f50bae1dfe09", // hash of "p.p" for testing
 "081a74595f90ee12c9885f8022919351",
-"1314c41591c5a7e2d33cb596f7b90896",
-"15087594c3b0e52c8d54f5304cc7ec3f",
-"21ac935c1e90efe82e29d4713f4258db",
+"120bdb07b34c6597742c090cabe63d8f",
+"220e79b001eced4cf4de996641b9ed98",
 "2b414e626d3369a3cbc09db841e4ad75",
-"3d99b44bd87da556a1c596a7ab28b9a5",
-"42c248ea0e93256bfc35f6db1477b751",
-"4c5b42fa85b10cc38fc447021d95b589",
-"60538dc94623543492479124f361bf74",
-"64845e0dec8ab59e35fb42999ed7c561",
-"790bf8229966ac6123e7d824711296f9",
-"833a4f00b5b7899de5cd04546515a2ef",
-"8945e049337dfb4f375de77c84ca9095",
-"98971604de800d47789f28c4c287f8e4",
-"9a40a9d84a8bedd3d97b9f3f8406db0f",
-"cada2934366802dda0bb05f45ac31a2f",
-"ce7cdcbbd16328dcade8aedf5a779995",
-"f77df6c8a9131cb8aa457734ad45f9d6",
-"f9e05a49b1c65b3b1f7daed40b42107c",
-"f9ff5c6d6df78cd4ec9592b82e0e18cd"];
+"7fa9d731110fb509a959ae3fc92e6f12",
+"a6be66d3b989948b5e056d8f3127c477",
+"cb7cb819b971c81380255caa55a3e3d4",
+"f9ff5c6d6df78cd4ec9592b82e0e18cd"
+];
